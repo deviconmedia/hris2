941cd41c775b50c3c29d2a44220a53acec45fb3a
Efron Paduansi <eufrondpaduansi@gmail.com>
2025-02-13 17:41:51 +0700

    update dashboard widgets

--- resources/views/adminpanel/home.blade.php
+++ resources/views/adminpanel/home.blade.php
@@ -7,41 +7,39 @@
 @section('content')
     <div class="col-12 col-lg-9">
         <div class="row">
-            <div class="col-6 col-lg-3 col-md-6">
+            <div class="col-6 col-lg-6 col-md-6">
                 <div class="card">
                     <div class="card-body px-4 py-4-5">
                         <div class="row">
                             <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                <div class="stats-icon purple mb-2">
-                                    <i class="iconly-boldShow"></i>
-                                </div>
+                                <h1> <i class="bi bi-people"></i></h1>
                             </div>
                             <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                <h6 class="text-muted font-semibold">Profile Views</h6>
+                                <h6 class="text-muted font-semibold">Checkin Hari ini</h6>
                                 <h6 class="font-extrabold mb-0">112.000</h6>
+                                <small class="text-muted">5/43 sudah checkin</small>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-6 col-lg-3 col-md-6">
+            <div class="col-6 col-lg-6 col-md-6">
                 <div class="card">
                     <div class="card-body px-4 py-4-5">
                         <div class="row">
                             <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                <div class="stats-icon blue mb-2">
-                                    <i class="iconly-boldProfile"></i>
-                                </div>
+                                <h1> <i class="bi bi-calendar-check"></i></h1>
                             </div>
                             <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                <h6 class="text-muted font-semibold">Followers</h6>
+                                <h6 class="text-muted font-semibold">Pengajuan Cuti</h6>
                                 <h6 class="font-extrabold mb-0">183.000</h6>
+                                <small class="text-muted">3/7 sudah disetujui</small>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-6 col-lg-3 col-md-6">
+            {{-- <div class="col-6 col-lg-3 col-md-6">
                 <div class="card">
                     <div class="card-body px-4 py-4-5">
                         <div class="row">
@@ -74,7 +72,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </div>
         <div class="row">
             <div class="col-12">
