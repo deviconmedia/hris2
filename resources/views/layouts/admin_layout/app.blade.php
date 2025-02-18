30d5bf267d22b8ac9505f41a63b6d211d37f7fec
Efron Paduansi <eufrondpaduansi@gmail.com>
2025-02-18 20:54:16 +0700

    fix mix content issue

--- resources/views/layouts/admin_layout/app.blade.php
+++ resources/views/layouts/admin_layout/app.blade.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- Mencegah mix content --}}
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') - {{ env('APP_NAME') }}</title>
 
--- resources/views/layouts/authentication_layout/app.blade.php
+++ resources/views/layouts/authentication_layout/app.blade.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        {{-- Mencegah mix content --}}
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="description" content="" />
         <meta name="author" content="" />
