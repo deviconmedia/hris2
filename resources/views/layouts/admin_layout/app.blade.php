b447a29431c649723135802b79281f9d01aa83db
Efron Paduansi <eufrondpaduansi@gmail.com>
2025-02-18 20:58:42 +0700

    update meta

--- resources/views/layouts/admin_layout/app.blade.php
+++ resources/views/layouts/admin_layout/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{-- Mencegah mix content --}}
+    {{-- Mencegah mix content ajax --}}
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
